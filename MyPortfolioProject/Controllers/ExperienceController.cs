81510686a4f870bb03ae813690039600d59b6f99
sebahatkilicoglu <103363734+sebahatkilicoglu@users.noreply.github.com>
2024-12-09 22:09:55 +0000

    ..

---
+++ MyPortfolioProject/Controllers/ExperienceController.cs
@@ -0,0 +1,55 @@
+﻿using MyPortfolioProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolioProject.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        DbMyPortfolioEntities context = new DbMyPortfolioEntities();
+        public ActionResult ExperienceList()
+        {
+            var values = context.Experiences.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateExperience()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateExperience(Experience experience)
+        {
+            context.Experiences.Add(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        public ActionResult DeleteExperience(int id)
+        {
+            var value = context.Experiences.Find(id);
+            context.Experiences.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+        [HttpGet]
+        public ActionResult UpdateExperience(int id)
+        {
+            var value = context.Experiences.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateExperience(Experience experience)
+        {
+            var value = context.Experiences.Find(experience.ExperienceId);
+            value.SubTitle = experience.SubTitle;
+            value.Title = experience.Title;
+            value.Description = experience.Description;
+            context.SaveChanges();
+            return RedirectToAction ("ExperienceList");
+        }
+    }
+}
---
+++ MyPortfolioProject/Controllers/MessageController.cs
@@ -0,0 +1,33 @@
+﻿using MyPortfolioProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolioProject.Controllers
+{
+    public class MessageController : Controller
+    {
+        DbMyPortfolioEntities context=new DbMyPortfolioEntities();
+        public ActionResult Inbox()
+        {
+            var values=context.Contacts.ToList();
+            return View(values);
+        }
+        public ActionResult ChangeMessageToTrue (int id)
+        {
+            var value =context.Contacts.Find(id);
+            value.IsRead = true;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+        public ActionResult ChangeMessageToFalse(int id)
+        {
+            var value = context.Contacts.Find(id);
+            value.IsRead = false;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+    }
+}
