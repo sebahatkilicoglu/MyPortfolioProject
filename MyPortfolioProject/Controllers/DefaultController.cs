7aeab6e22351a9cb620e410f92c4a2e77429a817
sebahatkilicoglu <103363734+sebahatkilicoglu@users.noreply.github.com>
2025-01-17 22:41:29 +0000

    ..

--- MyPortfolioProject/Controllers/DefaultController.cs
+++ MyPortfolioProject/Controllers/DefaultController.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MyPortfolioProject.Models;
 namespace MyPortfolioProject.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
+      DbMyPortfolioEntities context= new DbMyPortfolioEntities();
         public ActionResult Index()
         {
             return View();
@@ -27,5 +27,35 @@ namespace MyPortfolioProject.Controllers
             return PartialView();   
         }
 
+        public PartialViewResult PartialHeader() 
+        {
+            ViewBag.title= context.Profiles.Select(x=>x.Title).FirstOrDefault();
+            ViewBag.description = context.Profiles.Select(x=> x.Description).FirstOrDefault();
+            ViewBag.address = context.Profiles.Select(x=> x.Address).FirstOrDefault();
+            ViewBag.email = context.Profiles.Select(x=> x.Email).FirstOrDefault();
+            ViewBag.phone = context.Profiles.Select(x=> x.Phone).FirstOrDefault();
+            ViewBag.github = context.Profiles.Select(x=> x.Github).FirstOrDefault();
+            ViewBag.imageUrl = context.Profiles.Select(x=> x.ImageUrl).FirstOrDefault();
+
+            return PartialView();
+        }
+
+        public PartialViewResult PartialAbout()
+        {
+            var values = context.Abouts.ToList();
+            return PartialView(values);   
+        }
+
+        public PartialViewResult PartialExperience()
+        {
+            var values= context.Experiences.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialSkill()
+        { 
+            var values= context.Skills.Where(x=>x.Status==true).ToList();
+            return PartialView(values);        
+        }
     }
 }
