b7b165109b3851c22cbc8c282224de4507f116b2
sebahatkilicoglu <103363734+sebahatkilicoglu@users.noreply.github.com>
2025-01-18 12:25:50 +0000

    ..

--- MyPortfolioProject/Controllers/ContactController.cs
+++ MyPortfolioProject/Controllers/ContactController.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyPortfolioProject.Models;
 
 namespace MyPortfolioProject.Controllers
 {
     public class ContactController : Controller
     {
-        // GET: Contact
+       DbMyPortfolioEntities context= new DbMyPortfolioEntities();
         public PartialViewResult PartialContactSidebar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialContactDetail()
+        {
+            ViewBag.address= context.Profiles.Select(p => p.Address).FirstOrDefault();
+            ViewBag.description= context.Profiles.Select(p => p.Description).FirstOrDefault();
+            ViewBag.phone= context.Profiles.Select(p => p.Phone).FirstOrDefault();
+            ViewBag.email= context.Profiles.Select(p => p.Email).FirstOrDefault();
+            return PartialView();
+        }
+        public PartialViewResult PartialContactLocation() 
         {
             return PartialView();
         }
--- MyPortfolioProject/Controllers/DefaultController.cs
+++ MyPortfolioProject/Controllers/DefaultController.cs
@@ -8,10 +8,30 @@ namespace MyPortfolioProject.Controllers
 {
     public class DefaultController : Controller
     {
-      DbMyPortfolioEntities context= new DbMyPortfolioEntities();
+        DbMyPortfolioEntities context = new DbMyPortfolioEntities();
         public ActionResult Index()
         {
+            List<SelectListItem> values = (from x in context.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryId.ToString()
+                                           }).ToList();
+
+            ViewBag.v = values;
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Contact contact)
+        {
+            contact.SendDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            contact.IsRead = false;
+
+            context.Contacts.Add(contact);
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         public PartialViewResult PartialHead()
@@ -24,18 +44,18 @@ namespace MyPortfolioProject.Controllers
         }
         public PartialViewResult PartialNavbar()
         {
-            return PartialView();   
+            return PartialView();
         }
 
-        public PartialViewResult PartialHeader() 
+        public PartialViewResult PartialHeader()
         {
-            ViewBag.title= context.Profiles.Select(x=>x.Title).FirstOrDefault();
-            ViewBag.description = context.Profiles.Select(x=> x.Description).FirstOrDefault();
-            ViewBag.address = context.Profiles.Select(x=> x.Address).FirstOrDefault();
-            ViewBag.email = context.Profiles.Select(x=> x.Email).FirstOrDefault();
-            ViewBag.phone = context.Profiles.Select(x=> x.Phone).FirstOrDefault();
-            ViewBag.github = context.Profiles.Select(x=> x.Github).FirstOrDefault();
-            ViewBag.imageUrl = context.Profiles.Select(x=> x.ImageUrl).FirstOrDefault();
+            ViewBag.title = context.Profiles.Select(x => x.Title).FirstOrDefault();
+            ViewBag.description = context.Profiles.Select(x => x.Description).FirstOrDefault();
+            ViewBag.address = context.Profiles.Select(x => x.Address).FirstOrDefault();
+            ViewBag.email = context.Profiles.Select(x => x.Email).FirstOrDefault();
+            ViewBag.phone = context.Profiles.Select(x => x.Phone).FirstOrDefault();
+            ViewBag.github = context.Profiles.Select(x => x.Github).FirstOrDefault();
+            ViewBag.imageUrl = context.Profiles.Select(x => x.ImageUrl).FirstOrDefault();
 
             return PartialView();
         }
@@ -43,19 +63,25 @@ namespace MyPortfolioProject.Controllers
         public PartialViewResult PartialAbout()
         {
             var values = context.Abouts.ToList();
-            return PartialView(values);   
+            return PartialView(values);
         }
 
         public PartialViewResult PartialExperience()
         {
-            var values= context.Experiences.ToList();
+            var values = context.Experiences.ToList();
             return PartialView(values);
         }
 
         public PartialViewResult PartialSkill()
-        { 
-            var values= context.Skills.Where(x=>x.Status==true).ToList();
-            return PartialView(values);        
+        {
+            var values = context.Skills.Where(x => x.Status == true).ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
         }
     }
+
 }
